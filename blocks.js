738b71db9ac22f8b09346065eda9382b8e1e1593
tindoann <tindoann@gamil.com>
2019-12-19 11:29:55 -0500

    Day21a - White

--- blocks.js
+++ blocks.js
@@ -3725,3 +3725,20 @@ function repeat(str, n) {
 	}
 	return s;
 }
+
+// 139. Create a function that takes two arguments (item, times). 
+// The first argument (item) is the item that needs repeating while the second argument (times)
+//  is the number of times the item is to be repeated. Return the result in an array.
+
+// White -  repeat(3, 4) -> (3, 3, 3, 3)
+function repeat(item, times) {
+	return Array(times).fill(item);
+}
+
+function repeat(item, times) {
+	let a = [];
+	for (let i = 0; i<times; i++){
+		a.push(item)
+	}
+	return a
+}
