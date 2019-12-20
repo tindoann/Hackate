5da88b84a21d32603c6dcdb4dffe4515255a9d24
tindoann <tindoann@gamil.com>
2019-12-20 14:38:32 -0500

    Day21b - Yellow

--- blocks.js
+++ blocks.js
@@ -3916,3 +3916,30 @@ function sum(arr) {
 	return sum
 }
 
+// 146. Create a function that takes two strings as arguments and returns the number of times the first string is found in the second string. Regex
+// Yellow - charCount("b", "big fat bubble") ➞ 4
+
+function charCount(myChar, str) {
+	let arr = str.split('')
+	let count = arr.filter(x => x == myChar)
+	return count.length
+}
+
+function charCount(myChar, str) {
+	return [...str].filter(x => x === myChar).length;
+}
+
+function charCount(myChar, str) {
+	let count = 0;
+	for (let i=0; i<str.length; i++) {
+		if (str.charAt(i) === myChar) {
+			count++;
+		}
+	}
+	return count;
+}
+
+
+
+
+
