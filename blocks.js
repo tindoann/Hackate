606dd100aaf3a4949125b58f4ae35caee8046212
tindoann <tindoann@gamil.com>
2019-12-28 16:29:54 -0500

    Day28c - White

--- blocks.js
+++ blocks.js
@@ -4963,3 +4963,24 @@ function isValid(zip) {
 function isValid(zip) {
 	return !isNaN(zip);
 }
+
+// 177. Write a function that transforms all letters from [a, m] to 0 and letters from [n, z] to 1 in a string.
+
+// White - convertBinary("house") ➞ "01110"
+// convertBinary("excLAIM") ➞ "0100000"
+// convertBinary("moon") ➞ "0111"
+
+function convertBinary(str) {
+	return str
+					.replace(/[a-m]/gi, '0')
+					.replace(/[n-z]/gi, '1');
+}
+
+const convertBinary = str =>
+	str.replace(/[a-m]|([n-z])/gi, (match, g1) => Number(match === g1));
+	
+function convertBinary(str) {
+	var s = /[a-mA-M]/;
+	return str.split("").map(x => x.match(s) ? 0 :  1).join("");
+}
+
