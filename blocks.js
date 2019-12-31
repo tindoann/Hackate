91552dbdfd8cf59f72d4dd4829efe8603d29ba9e
tindoann <tindoann@gamil.com>
2019-12-30 19:52:34 -0500

    Day31e - Yellow

--- blocks.js
+++ blocks.js
@@ -5351,3 +5351,24 @@ function transformUpvotes(str) {
 	}
 	return str.map(x => Number(x));
 }
+
+// 190. Create a function that takes an array of 10 numbers (between 0 and 9) and returns a string of those numbers formatted as a phone number (e.g. (555) 555-5555).
+
+// Yellow - formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
+// formatPhoneNumber([5, 1, 9, 5, 5, 5, 4, 4, 6, 8]) ➞ "(519) 555-4468"
+// formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7]) ➞ "(345) 501-2527"
+
+function formatPhoneNumber(numbers) {
+  return `(${numbers.slice(0,3).join("")}) ${numbers.slice(3,6).join("")}-${numbers.slice(6).join("")}`
+}
+
+function formatPhoneNumber(numbers) {
+  const first = numbers.slice(0, 3).join('')
+  const middle = numbers.slice(3,6).join('')
+  const last = numbers.slice(6).join('')
+  return `(${first}) ${middle}-${last}`
+}
+
+function formatPhoneNumber(numbers) {
+  return numbers.join('').replace(/^(\d{3})/, '($1) ').replace(/(\d{3})(\d{4})$/, '$1-$2');
+}
