b0da7336d5b44ae72c67db5c12c6106b63067b4e
tindoann <tindoann@gamil.com>
2020-01-03 18:27:47 -0500

    Day35b - White

--- blocks.js
+++ blocks.js
@@ -5758,4 +5758,17 @@ let detectBrowser = userAgent => userAgent.includes("Firefox")
 
 detectBrowser = s => s.includes("Firefox") ? "Mozilla Firefox" : s.includes("AppleWebKit") ? "Google Chrome" : "Internet Explorer"
 
+// 202. Write a function redundant that takes in a string str and returns a function that returns str.
+
+// White - const f1 = redundant("apple")
+// f1() ➞ "apple"
+
+// const f2 = redundant("pear")
+// f2() ➞ "pear"
+
+// const f3 = redundant("")
+// f3() ➞ ""
+
+const redundant = str => () => str;
+
 
