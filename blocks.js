3c2bd03a9a6b884dab82965dd01004f209ff2d36
tindoann <tindoann@gamil.com>
2020-01-06 23:38:55 -0500

    Day37b - White

--- blocks.js
+++ blocks.js
@@ -6026,3 +6026,39 @@ function keysAndValues(obj) {
   return result
 }
 
+// 212. Create a function that takes two numbers and a mathematical operator + - / * and will perform a calculation with the given numbers.
+
+function calculator(num1,operator,num2) {
+	return {
+		'+': num1 + num2,
+		'-': num1 - num2,
+		'*': num1 * num2,
+		'/': num2 ? num1 / num2 : 'Cannot divide by 0!'
+	}[operator]
+}
+
+function calculator(num1, operator, num2) {
+	operations = {
+		'+': (a,b) => a + b,
+		'-': (a,b) => a - b,
+		'*': (a,b) => a * b,
+		'/': (a,b) => b ? a / b :"Can't divide by 0!"
+	}
+	
+	return operations[operator](num1,num2)
+}
+
+function calculator(num1, operator, num2) {
+	switch (operator) {
+		case '+':
+			return num1 + num2;
+		case '-':
+			return num1 - num2;
+		case '*':
+			return num1 * num2;
+		case '/':
+			if (num2 === 0) return "Can't divide by 0!";
+			return num1 / num2;
+	}
+	return null;
+}
