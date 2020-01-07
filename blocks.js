29499cefe15e3e3d454f8ceeba1d6c3f0f47adf6
tindoann <tindoann@gamil.com>
2020-01-07 15:54:04 -0500

    Day38a - White

--- blocks.js
+++ blocks.js
@@ -6062,3 +6062,34 @@ function calculator(num1, operator, num2) {
 	}
 	return null;
 }
+
+// 213. Create a function that takes an array of numbers and returns a new array, sorted in ascending order (smallest to biggest).
+
+// White - Sort numbers array in ascending order.
+// If functions argument is null, an empty array or undefined, return an empty array.
+// Return new array of sorted numbers.
+
+// sortNumsAscending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
+// sortNumsAscending([80, 29, 4, -95, -24, 85]) ➞ [-95, -24, 4, 29, 80, 85]
+// sortNumsAscending(null) ➞ []
+// sortNumsAscending([]) ➞ []
+
+function sortNumsAscending(arr) {
+  return (arr || []).sort((a,b) => a - b)
+}
+
+function sortNumsAscending(arr) {
+  if (arr === null) return [];
+  return arr.sort((a, b) => a - b);
+}
+
+function sortNumsAscending(arr) {
+  if (!arr) return [];
+  return arr.sort((a,b) => a - b);
+}
+
+function sortNumsAscending(arr) {
+  return arr ? arr.sort(function(a, b){ return a - b; }) : [];
+}
+
+
