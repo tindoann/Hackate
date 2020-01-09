0c18b78cf5a3009a7c9021a96b46ab06a9bb53db
tindoann <tindoann@gamil.com>
2020-01-08 20:25:59 -0500

    Day38d - Yellow

--- blocks.js
+++ blocks.js
@@ -6354,3 +6354,37 @@ function reverseImage(image) {
 	}
 	return image;
 }
+
+// 222. A quadratic equation a x² + b x + c = 0 has either 0, 1, or 2 distinct solutions for real values of x. 
+// Given a, b and c, you should return the number of solutions to the equation.
+
+// Yellow - solutions(1, 0, -1) ➞ 2
+// x² - 1 = 0 has two solutions (x = 1 and x = -1).
+
+// solutions(1, 0, 0) ➞ 1
+// x² = 0 has one solution (x = 0).
+
+// solutions(1, 0, 1) ➞ 0
+// x² + 1 = 0 has no solutions.
+
+function solutions(a, b, c) {
+	var d = Math.pow(b,2) - 4*a*c;
+	if (d > 0) return 2;
+	if (d == 0) return 1;
+	return 0;
+}
+
+function solutions(a,b,c) {
+  const disc = b*b - 4*a*c;
+  return disc > 0 ? 2 : disc == 0 ? 1 : 0;
+}
+
+function solutions(a, b, c) {
+	let num = Math.pow(b, 2) - 4 * a * c;
+	return num === 0 ? 1 : (num > 0 ? 2 : 0);
+}
+
+const solutions = (a,b,c) => [0,1,2][Math.sign(b*b-4*a*c)+1];
+
+
+
