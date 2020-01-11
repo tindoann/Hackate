a9119dab7033059ae9dc0e38f5e1089b2d3f1efa
tindoann <tindoann@gamil.com>
2020-01-10 23:58:14 -0500

    Day40d - White

--- blocks.js
+++ blocks.js
@@ -6578,3 +6578,19 @@ function formatNum(num) {
   }
   return ar.join('');
 }
+
+// 330. Create a function that takes an array of items, removes all duplicate items and returns a new array in the same sequential order as the old array (minus duplicates).
+
+// White - removeDups([1, 0, 1, 0]) ➞ [1, 0]
+// removeDups(["The", "big", "cat"]) ➞ ["The", "big", "cat"]
+// removeDups(["John", "Taylor", "John"]) ➞ ["John", "Taylor"]
+
+const removeDups = arr => [...new Set(arr)]
+
+function removeDups(arr) {
+  return [...new Set(arr)];
+}
+
+function removeDups(arr) {
+  return arr.filter((x, i, a) => a.indexOf(x) === i);
+}
