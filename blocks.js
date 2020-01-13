0f5fa90fe4c507308c63fe614b4898de10be829f
tindoann <tindoann@gamil.com>
2020-01-13 18:32:50 -0500

    Day42b - Yellow

--- blocks.js
+++ blocks.js
@@ -6893,3 +6893,43 @@ const getMiddle = str => {
 	return str.slice(middle - (str.length % 2 ? 0 : 1), middle + 1);
 }
 
+// 441. To train for an upcoming marathon, Johnny goes on one long-distance run each Saturday. 
+// He wants to track how often the number of miles he runs this Saturday exceeds the number of miles run the previous Saturday. This is called a progress day.
+
+// Create a function that takes in an array of miles run every Saturday and returns Johnny's total number of progress days.
+
+// Yellow - progressDays([3, 4, 1, 2]) ➞ 2
+// There are two progress days, (3->4) and (1->2)
+// progressDays([10, 11, 12, 9, 10]) ➞ 3
+// progressDays([6, 5, 4, 3, 2, 9]) ➞ 1
+// progressDays([9, 9])  ➞ 0
+
+function progressDays(runs) {
+	let days = 0; 
+	for (let i = 1; i < runs.length; i++) {
+		if (runs[i] > runs[i -1]){
+			days++; 
+		}
+	}
+	return days; 
+}
+
+function progressDays(runs) {
+	var days=0;
+	for(i =1; i<runs.length; i++){
+		if(runs[i]>runs[i-1]){
+			days++;
+		} 
+	}
+	return days;
+}
+
+function progressDays(runs) {
+	return runs.filter((e, i, a) => e < a [i + 1]).length; 
+}
+
+const progressDays = runs =>
+  runs.slice(1).reduce((progress, runs, i) => {
+		const prevRun = runs[i]; 
+		return progress + (run > prevRun ? 1 : 0); 
+	}, 0); 
