770088d91cd7369d6c5b559fef997e8bd360e7a3
tindoann <tindoann@gamil.com>
2020-01-13 18:09:30 -0500

    Day43a - Yellow

--- blocks.js
+++ blocks.js
@@ -6860,3 +6860,36 @@ function mean(num) {
 	let nums = arr.map((val) => Number(val));
 	return nums.reduce((acc, next) => acc + next) / nums.length;
 }
+
+// 440. Create a function that takes a string and returns the middle character(s). If the word's length is odd, return the middle character. If the word's length is even, return the middle two characters.
+
+// Yellow - getMiddle("test") ➞ "es"
+// getMiddle("testing") ➞ "t"
+// getMiddle("middle") ➞ "dd"
+// getMiddle("A") ➞ "A"
+
+function getMiddle(str) {
+  var start = Math.ceil(str.length / 2) - 1;
+  var end = Math.floor(str.length / 2) + 1;
+  return str.substring(start, end);
+}
+
+function getMiddle(str) {
+  return str.length % 2 ? str[(str.length - 1) / 2] : str.substr((str.length - 2) / 2 , 2);
+}
+
+function getMiddle(str) {
+  var length = Math.floor(str.length/2);
+  if (str.length % 2 === 0){
+    var newStr = '';
+    return newStr += str[length-1] + str[length]
+  } else {
+    return str[length]
+  }
+}
+
+const getMiddle = str => {
+	const middle = str.length / 2 | 0;
+	return str.slice(middle - (str.length % 2 ? 0 : 1), middle + 1);
+}
+
