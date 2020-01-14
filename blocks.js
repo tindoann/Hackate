8710aaa82378a1677314c675bf838db4782f10ac
tindoann <tindoann@gamil.com>
2020-01-14 17:58:41 -0500

    Day44d - Yellow

--- blocks.js
+++ blocks.js
@@ -7060,7 +7060,7 @@ function maskify(str) {
 
 // 446. Create a function that returns the total number of steps it takes to transform each element to the maximal element in the array. Each step consists of incrementing a digit by one.
 
-// incrementToTop([3, 4, 5]) ➞ 3
+// Yellow - incrementToTop([3, 4, 5]) ➞ 3
 // 3 increments: 3 -> 4, 4 -> 5; 4 -> 5
 // incrementToTop([4, 3, 4]) ➞ 1
 // incrementToTop([3, 3, 3]) ➞ 0
@@ -7094,3 +7094,24 @@ function incrementToTop(arr) {
   }
   return counter;
 }
+
+// 447. Write a function that takes an array and returns a new array with unique positive (more than 0) numbers.
+
+// Yellow - uniqueArr([-5, 1, -7, -5, -2, 3, 3, -5, -1, -1]) ➞ [1, 3]
+// uniqueArr([3, -3, -3, 5, 5, -6, -2, -4, -1, 3]) ➞ [3, 5]
+// uniqueArr([10, 6, -12, 13, 5, 5, 13, 6, 5]) ➞ [10, 6, 13, 5]
+
+const uniqueArr = arr => [...new Set(arr.filter(v => v > 0))]
+
+function uniqueArr(arr) {
+	let set = Array.from(new Set(arr));
+	return set.filter(x => x > 0);
+}
+
+function uniqueArr(arr) {
+	return arr.filter(x => x > 0).filter((x, i, a)=> i == a.indexOf(x));
+}
+
+
+
+
