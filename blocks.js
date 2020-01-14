7e1b9be845def45d8c6c75e438d7990a990d4949
tindoann <tindoann@gamil.com>
2020-01-14 12:37:21 -0500

    Day43a - White

--- blocks.js
+++ blocks.js
@@ -7006,3 +7006,28 @@ function missingNum(arr) {
     if(!arr.includes(i)) return i;
   }
 }
+
+// 444. ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns true if the PIN is valid and false if it's not.
+
+function validatePIN(pin) {
+  return (/^(\d{4}|\d{6})$/).test(pin);
+}
+
+function validatePIN(pin) {
+  if (!parseInt(pin).isNaN){
+    if (pin.length == 4 || pin.length == 6){
+      return true;
+    }
+  }
+  return false;
+}
+
+function validatePIN(pin) {
+  var regex = /^(\d{4})$|^(\d{6})$/;
+	return regex.test(pin);
+}
+
+function validatePIN(pin) {
+  var regEx =  new RegExp(/^(\d{4}|\d{6})$/);
+  return regEx.test(pin);
+}
