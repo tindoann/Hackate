89429d336ccefb457ee25889884b837889a4ef87
tindoann <tindoann@gamil.com>
2020-01-14 19:44:25 -0500

    Day43f - Yellow

--- blocks.js
+++ blocks.js
@@ -7143,3 +7143,33 @@ function nextInLine(arr, num) {
 function nextInLine(arr, num) {
 	return !arr.length ? 'No array has been selected' : [...arr].concat(num).slice(1)
 }
+
+// 449. Additional spaces have been added to a sentence. Return the correct sentence by removing them. All words should be separated by one space, and there should be no spaces at the beginning or end of the sentence.
+
+// Yellow - correctSpacing("The film   starts       at      midnight. ")
+// ➞ "The film starts at midnight."
+
+// correctSpacing("The     waves were crashing  on the     shore.   ")
+// ➞ "The waves were crashing on the shore."
+
+// correctSpacing(" Always look on    the bright   side of  life.")
+// ➞ "Always look on the bright side of life."
+
+function correctSpacing(sentence) {
+	return sentence.replace(/\s+/g,' ').trim();
+}
+
+function correctSpacing(sentence) {
+  return sentence.split(" ").filter(a=>a!="").join(" ")
+}
+
+function correctSpacing(sentence) {
+	let arr = sentence.match(/\S+/g);
+	return arr.join(" ");
+}
+
+function correctSpacing(sentence) {
+	return sentence.split(' ').filter(x => x != '').join(' ')
+}
+
+
