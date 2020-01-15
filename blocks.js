afc021fd79125bb9f2051128578aae4c9b06fbad
tindoann <tindoann@gamil.com>
2020-01-14 20:40:18 -0500

    Dayg - White

--- blocks.js
+++ blocks.js
@@ -7209,3 +7209,18 @@ function stepsToConvert(str) {
 	return Math.min((str.match(/[a-z]/g) || []).length, (str.match(/[A-Z]/g) || []).length)
 }
 
+// 451. Create a function that accepts a string and returns true if it's in the format of a proper phone number and false if it's not. 
+// Assume any number between 0-9 (in the appropriate spots) will produce a valid phone number. This is what a valid phone number looks like: (123) 456-7890
+
+// Yellow - isValidPhoneNumber("(123) 456-7890") ➞ true
+// isValidPhoneNumber("1111)555 2345") ➞ false
+// isValidPhoneNumber("098) 123 4567") ➞ false
+
+function isValidPhoneNumber(str) {
+	return /^\(\d{3}\) \d{3}-\d{4}$/.test(str);
+}
+
+function isValidPhoneNumber(str) {
+  var expr=/^\(\d{3}\)\s\d{3}\-\d{4}$/g;
+	return expr.test(str);
+}
