67147f89664c61eedda9c24879ab464709069034
tindoann <tindoann@gamil.com>
2020-01-16 12:05:12 -0500

    Day45h - Yellow

--- blocks.js
+++ blocks.js
@@ -7627,4 +7627,27 @@ function isLastCharacterN(word) {
 	}
 }
 
+// 556. Write a function that returns the greatest common divisor (GCD) of two integers.
+
+// White - gcd(32, 8) ➞ 8
+// gcd(8, 12) ➞ 4
+// gcd(17, 13) ➞ 1
+
+const gcd = (n1, n2) => !n2 ? n1: gcd(n2, n1 % n2)
+
+function gcd(n1, n2) {
+	if (n2 == 0) return n1
+	  return gcd(n2, n1 % n2); 
+}
+
+const gcd = (a, b) => (!b ? a : gcd(b, a % b)); 
+
+function gcd(n1, n2) {
+	let resolve = 0; 
+	  for (let i = 0; i < Math.min(n1, n2); i++)
+			if (n1% i == 0 && n2% i ==0) resolve = i; 
+  return resolve
+}
+
+
 
