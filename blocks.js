dc4e8c646c44ea2dfc0eb639601875ef30b3f590
tindoann <tindoann@gamil.com>
2020-01-16 11:28:47 -0500

    Day45f - Yellow

--- blocks.js
+++ blocks.js
@@ -7570,3 +7570,31 @@ function findZip(str) {
 	return str.indexOf('zip', str.indexOf('zip')+3)
 }
 
+// Write a function that takes three arguments (x, y, z) and returns an array containing x subarrays (e.g. [[], [], []]), each containing y number of item z.
+
+// x Number of subarrays contained within the main array.
+// y Number of items contained within each subarray.
+// z Item contained within each subarray.
+
+function matrix(x, y, z) {
+	return Array(x).fill(Array(y).fill(z)); 
+}
+
+const matrix = (x, y, z) => 
+	Array.from({length: x}, () => 
+		Array.from({length: y}, () => z)
+	); 
+
+function matrix(x, y, z) {
+	return Array(x).fill().map(m => Array(y).fill(z)); 
+}
+
+function matrix(x, y, z)  {
+	let arr = []; 
+	for (let i = 0; i < x; i += 1) {
+		let subarr = new Array(y); 
+		subarr.fill(z, 0, y); 
+		arr.push(subarr); 
+	}
+	return arr; 
+}
