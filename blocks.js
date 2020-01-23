69fbde586367f4c15897e05a571bb00579caa07c
tindoann <tindoann@gamil.com>
2020-01-22 19:58:44 -0500

    Day49e - White

--- blocks.js
+++ blocks.js
@@ -8710,3 +8710,27 @@ const censor = str => {
 		.join(" ");
 }
 
+// 675. Write a function that takes the last number of a consecutive list of numbers and returns the total of all numbers up to and including it.
+
+// addUpTo(3) ➞ 6
+// 1 + 2 + 3 = 6
+// addUpTo(10) ➞ 55
+// 1 + 2 + 3 + ... + 10 = 55
+// addUpTo(7) ➞ 28
+// 1 + 2 + 3 + ... + 7 = 28
+
+const addUpTo = n => n * (n + 1) / 2; 
+
+function addUpTo(n) {
+	let x = n * (n + 1); 
+	let y = x / 2; 
+	return y; 
+}
+
+function addUpTo(n) {
+	return (n * (n + 1)) / 2; // 3 --> 6
+}
+
+function addUpTo(n) {
+	return n == 0 ? 0 : n + addUpTo(n - 1); 
+}
