89f5e4e6e8f1bd04b9e35e9b8c0ea5a2c433365f
tindoann <tindoann@gamil.com>
2020-02-01 12:28:26 -0500

    Day58a - yellow

--- blocks.js
+++ blocks.js
@@ -10335,4 +10335,10 @@ function flip(str, spec) {
 	}
 }
 
+// 916 Given an array of integers, find the pair of adjacent elements that have the largest product and return that product.
 
+// yellow - adjacentProduct([3, 6, -2, -5, 7, 3] ) ➞ 21
+
+// adjacentProduct([5, 6, -4, 2, 3, 2, -23]) ➞ 30
+
+// adjacentProduct([0, -1, 1, 24, 1, -4, 8, 10]) ➞ 80
