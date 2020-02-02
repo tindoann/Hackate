1b1b341a7385d405edcaab69785f55092ae57958
tindoann <tindoann@gamil.com>
2020-02-02 16:58:21 -0500

    Day59b - White

--- blocks.js
+++ blocks.js
@@ -10463,4 +10463,10 @@ function getDiscounts(nums, d) {
 	return nums.map(x => x * newD / 100);
 }
 
+// 920. Given a fraction as a string, return whether or not it is greater than 1 when evaluated.
 
+// White - greaterThanOne("1/2") ➞ false
+
+// greaterThanOne("7/4") ➞ true
+
+// greaterThanOne("10/10") ➞ false
