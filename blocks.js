522d6a74bee8da3a32329238c82fc2f10dd364cb
tindoann <tindoann@gamil.com>
2020-02-02 20:32:35 -0500

    Day59e - Yellow

--- blocks.js
+++ blocks.js
@@ -10543,6 +10543,23 @@ function returnOnlyInteger(arr) {
 	return newArr;
 }
 
+// 923. Create a function that returns the thickness (in meters) of a piece of paper after folding it n number of times. The paper starts off with a thickness of 0.5mm.
+
+// White - numLayers(1) ➞ "0.001m"
+// Paper folded once is 1mm (equal to 0.001m)
+
+// numLayers(4) ➞ "0.008m"
+// Paper folded 4 times is 8mm (equal to 0.008m)
+
+// numLayers(21) ➞ "1048.576m"
+// Paper folded 21 times is 1048576mm (equal to 1048.576m)
+
+const rnd = n => n > 1e16 ? n.toExponential() :
+	(Number.isInteger(n) && !(''+n).includes('+')) ? n+'.0' : n;
+const numLayers = n => rnd(2**n / 2000) + 'm';
+
+
+
 
 
 
