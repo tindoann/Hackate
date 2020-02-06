d9edb94d473c5732eba3a6ceee3f8ebbed738b89
tindoann <tindoann@gamil.com>
2020-02-06 13:48:05 -0500

    Day62a - White

--- blocks.js
+++ blocks.js
@@ -10947,4 +10947,29 @@ const countSmileys = a => {
 	return x === null ? 0 : x.length;
 }
 
+// 936. Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
 
+// White - addUp(4) ➞ 10
+// addUp(13) ➞ 91
+// addUp(600) ➞ 180300
+
+function addUp(num) {
+  return (num * (num + 1))/2;
+}
+
+function addUp(num) {
+  if (num === 1) return 1;
+  return num + addUp(num - 1);
+}
+
+function addUp(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+const addUp = n => Array
+	.from({ length: n + 1 }, (v, i) => i)
+	.reduce((a, b) => a + b);
