3cf42ed53e620e8b8835c218922f98226ffe850e
tindoann <tindoann@gamil.com>
2020-02-05 19:10:21 -0500

    Day61c - Yellow

--- blocks.js
+++ blocks.js
@@ -10860,3 +10860,10 @@ function stripSentence(str, chars) {
 
 const stripSentence = (str, ch) => str.replace(RegExp(`[${ch}]`,`gi`),``)
 
+// 934. Given a fraction frac (given in the format "1/2" for example) and n number of decimal places, return a sentence in the following format:
+
+// "{fraction} rounded to {n} decimal places is {answer}"
+
+// Yellow - fracRound("1/3", 5) ➞ "1/3 rounded to 5 decimal places is 0.33333"
+// fracRound("2/8", 4) ➞ "2/8 rounded to 4 decimal places is 0.2500"
+// fracRound("22/7", 2) ➞ "22/7 rounded to 2 decimal places is 3.14"
