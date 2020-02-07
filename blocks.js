1364ddfd0122dabbf51f6a4005afdcdc35342648
tindoann <tindoann@gamil.com>
2020-02-07 18:37:45 -0500

    Day63b - Yellow

--- blocks.js
+++ blocks.js
@@ -11115,3 +11115,52 @@ function mergeArrays(a, b) {
     a.push( b[i + 1] );
 	}
 }
+
+// 942. You will be given a number and you will need to return it as a string in Expanded Form.
+
+// Yellow - console.log( expandedForm(12) ); // Should return '10 + 2'
+// console.log( expandedForm(42) ); // Should return '40 + 2'
+// console.log( expandedForm(70304) ); // Should return '70000 + 300 + 4'
+// console.log( expandedForm(9000000) );
+
+function expandedForm(num) {
+	// Convert num to a string array
+	let numStringArray = String(num).split('');
+	// Get length ofstring array
+	let len = numStringArray.length; 
+	let result = '';  
+
+	// For each digit in array
+	numStringArray.map( (n, index) => {
+		// Perform only if n > 0
+		if ( n > 0) {
+			// Add plus sign if result is not empty (for the next digits)
+			if (result) { result += ' + '}
+
+			// Pad zeros the right limited to array length minus current index
+			result += n.padEnd(len - index, '0'); 
+		}
+	}); 
+	return result; 
+}
+
+function expandedForm(num) {
+	return String(num)
+					// split the number
+					.split("")
+					// go over each index and subtract the length by 1 
+					.map((num, index, arr) => num + "0".repeat(arr.length - index -1 ))
+					// filter out the zero 
+					.filter((num) => Number(num) != 0)
+					// last, add the char together
+          .join(" + ")
+}
+
+function expandedForm(num) {
+  var arr = num.toString().split('').reverse();
+  var result = [];
+  for(var i = 0; i < arr.length; i++){
+    arr[i] == 0 ? result.push() : result.push(arr[i] + ('0'.repeat(i)))
+  }
+  return result.reverse().join(' + ')
+}
