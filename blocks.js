e2a54bc6152368f181f1ca1255b595a892d1bf8a
tindoann <tindoann@gamil.com>
2020-02-07 20:27:50 -0500

    Day63c - Yellow

--- blocks.js
+++ blocks.js
@@ -11164,3 +11164,20 @@ function expandedForm(num) {
   }
   return result.reverse().join(' + ')
 }
+
+// 943. Your goal in this kata is to implement an difference function, which subtracts one list from another.
+// It should remove all values from list a, which are present in list b.
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// Yellow - console.log( array_diff([1,2],[1]) ); //== [2]
+// console.log( array_diff([1,2,2,2,3],[2]) ); //== [1,3]
+
+function array_diff(a, b) {
+	// use array filter to check if every num in array a can be found in array b
+	return a.filter(function(num) {
+		// If num is not found in array b, return num
+		if (!b.includes(num)) {
+			return num; 
+		}
+	}); 
+}
