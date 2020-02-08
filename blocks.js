7617c1e6204b948e287b6643a4a672157a6c8ed6
tindoann <tindoann@gamil.com>
2020-02-08 18:06:44 -0500

    Day64d - White

--- blocks.js
+++ blocks.js
@@ -11237,3 +11237,13 @@ function removeChar(str) {
 
 	return str_array.join(''); 
 }; 
+
+// 947. Your task is to make a function that can take any non-negative integer as a argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// White - console.log( descendingOrder(21445) ); // Output: 54421
+// console.log( descendingOrder(145263) ); // Output: 654321
+// console.log( descendingOrder(1254859723) ); // Output: 9875543221
+
+function descendingOrder(n) {
+	return parseInt(n.toString().split('').sort().reverse().join(''));
+}
