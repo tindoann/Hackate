bcd6ea116bb804d69d5a496eac299be29aec1f94
tindoann <tindoann@gamil.com>
2020-02-09 17:17:11 -0500

    Day65d - Yellow

--- blocks.js
+++ blocks.js
@@ -11420,3 +11420,25 @@ function solve(s) {
 	}
 	return biggestNumber; 
 }
+
+// 955. Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
+
+// Trailing Zeros are the zeros after a decimal point which don't affect the value (e.g. the last three zeros in 3.4000 and 3.04000).
+// Leading Zeros are the zeros before a whole number which don't affect the value (e.g. the first three zeros in 000234 and 000230).
+
+// Yellow - removeLeadingTrailing("230.000") ➞ "230"
+// removeLeadingTrailing("00402") ➞ "402"
+// removeLeadingTrailing("03.1400") ➞ "3.14"
+// removeLeadingTrailing("30") ➞ "30"
+
+function removeLeadingTrailing(n) {
+	return Number(n)
+}
+
+function removeLeadingTrailing(n) {
+	return Math.abs(n); 
+}
+
+function removeLeadingTrailing(n) {
+	return parseFloat(n).toString(); 
+}
