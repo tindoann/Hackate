d9268c284ed2fda263abd515ecdbc98f8b568a99
tindoann <tindoann@gamil.com>
2020-02-10 15:09:40 -0500

    Day66a - Yellow

--- blocks.js
+++ blocks.js
@@ -11442,3 +11442,82 @@ function removeLeadingTrailing(n) {
 function removeLeadingTrailing(n) {
 	return parseFloat(n).toString(); 
 }
+
+// 956. Two strings a and b are called isomorphic if there is a one to one mapping possible for every character of a to every character of b. And all occurrences of every character in a map to same character in b.
+
+function isomorph(a, b) {
+  if( a.length !== b.length ) return false  // a and b should be of the same length
+  
+  // Every letter in word1 should be unique to word2
+  function checkUniqueMapping(word1, word2) {
+    let words = {}
+    return word1.split('').map( (letter, i) => {
+      // If letter is not found in words, create new mapping
+      if ( !(letter in words) ) {
+        words[letter] = word2.charAt(i)
+        return true
+      }
+      // If a mapping has been done before, ensure that it is the same mapping
+      else if( letter in words && words[letter] == word2.charAt(i)) return true
+      // Otherwise, the mapping is not unique
+      else return false
+    })
+  }
+
+  if(checkUniqueMapping(a, b).indexOf(false)>-1) return false // Check a against b
+  if(checkUniqueMapping(b, a).indexOf(false)>-1) return false // Check b against a
+  else return true
+}
+
+console.log( isomorph('CBAABC', 'DEFFED') )
+console.log( isomorph('RAMBUNCTIOUSLY', 'THERMODYNAMICS') )
+console.log( isomorph('AB', 'CC') )
+console.log( isomorph('ABAB', 'CD') )
+console.log( isomorph("SEE", "SAW") )
+
+// 957. // const {assert} = require("chai");
+
+// describe("sumNested", function() {
+//   it("should handle non-nested lists", function() {
+//     assert.strictEqual(sumNested([1]), 1);
+//     assert.strictEqual(sumNested([1, 2, 3, 4]), 10);
+//     assert.strictEqual(sumNested([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 55);
+//   });
+//   it("non-nested edge case", function() {
+//     assert.strictEqual(sumNested([]), 0);
+//   });
+//   it("should handle simple nestings", function() {
+//     assert.strictEqual(sumNested([[1], []]), 1);
+//     assert.strictEqual(sumNested([[1, 2, 3, 4]]), 10);
+//   });
+//   it("should handle more complex nestings", function() {
+//     assert.strictEqual(sumNested([1, [1], [[1]], [[[1]]]]), 4);
+//     assert.strictEqual(sumNested([1, [1], [1, [1]], [1, [1], [1, [1]]]]), 8);
+//   });
+//   it("complex nesting edge case", function() {
+//     assert.strictEqual(sumNested([[[[], [], [[[[[[[[[[]]]]]]]]]]], [], [], [[[], [[]]]]], []]), 0);
+//   });
+// });
+
+// Yellow - console.log(sumNested([1, [2, [3, [4]]]]))
+
+const sumNested = arr => {
+  // Flatten array until every element is not an array
+  arr = flatten(arr)
+  // Return the sum of the array
+  return arr.reduce( (sum,value) => sum + value, 0 )
+};
+
+// Flatten array until every element is not an array
+function flatten(arr) {
+  const flattened = arr.reduce( (a, b) => a.concat(b), [] );
+  if (objectInArray(flattened)) return flatten(flattened)
+  return flattened
+}
+
+// Return true if there is an array in the array
+function objectInArray(arr) {
+  let objectFound = false
+  arr.map( a => { if(typeof a==='object') objectFound = true })
+  return objectFound
+}
