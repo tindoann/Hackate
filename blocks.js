abe1d75f11f48328a04f4922886a06c8f83d3671
tindoann <tindoann@gamil.com>
2020-02-16 20:25:07 -0500

    Day72d - Yellow

--- blocks.js
+++ blocks.js
@@ -12406,3 +12406,21 @@ var doc = "hello"
 function callback(str) {
 	doc = str
 }
+
+// 984. Write a function that takes in a word and splits the consonants one by one, but keeps the vowels in a cluster.
+
+// Yellow - split("beautifully") ➞ ["b", "eau", "t", "i", "f", "u", "l", "l", "y"]
+// split("spoonful") ➞ ["s", "p", "oo", "n", "f", "u", "l"]
+// split("swimming") ➞ ["s", "w", "i", "m", "m", "i", "n", "g"]
+
+function split(word) {
+	return word.match(/([^aeiou] | [aeiou]+)/g)
+}
+
+function split(word) {
+	return word.split(/([aeiou]*)/g).filter(x => x !=='')
+}
+
+function split(word) {
+	word.replace(/[^aeiou]|[aeiou]+/g, v => `.${v}`).slice(1).split('.');
+}
