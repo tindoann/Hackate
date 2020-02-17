4923e26baa8768ca0ef5d7a5fff993a71b997fc6
tindoann <tindoann@gamil.com>
2020-02-17 18:28:19 -0500

    Day73c - Yellow

--- blocks.js
+++ blocks.js
@@ -12549,3 +12549,42 @@ function getStudentTopNotes(students) {
 	}
 	});
 }
+
+// 989. Create a function that tests whether or not an integer is a perfect number. A perfect number is a number that can be written as the sum of its factors, excluding the number itself.
+// For example, 6 is a perfect number, since 1 + 2 + 3 = 6, where 1, 2, and 3 are all factors of 6. Similarly, 28 is a perfect number, since 1 + 2 + 4 + 7 + 14 = 28.
+
+// Yellow - checkPerfect(6) ➞ true
+// checkPerfect(28) ➞ true
+// checkPerfect(496) ➞ true
+// checkPerfect(12) ➞ false
+// checkPerfect(97) ➞ false
+
+function checkPerfect(num) {
+	let factors = []; 
+	for(let i = i; i < num - 1; i ++) {
+		if (num % i === 0) {
+			factors.push(i); 
+		}
+	}
+	return factors.reduce((accum, current) => accum + current) === num;
+}
+
+function checkPerfect(num) {
+	let factors = []; 
+	for(let i = 1; i < num; i++) {
+		if (num % i == 0) {
+			factors.push(i)
+		}
+	}
+	return factors.reduce((a, b) => a + b) == num
+}
+
+function checkPerfect(num) {
+	let sum = 0
+	for (let i = 1; i < num; i++) {
+		if ((num % i) === 0) {
+			sum += i
+		}
+	}
+	return sum === num
+}
