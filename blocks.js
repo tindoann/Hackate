da8beaa0facf1a70afa36c71486793327a95ed93
tindoann <tindoann@gamil.com>
2020-02-19 17:20:42 -0500

    Day74b - Yellow

--- blocks.js
+++ blocks.js
@@ -12718,4 +12718,42 @@ function freeThrows(success, throws) {
 	return Math.pow(success.slice(0, -1)/100, throws).toFixed(2) * 100 + '%'
 }
 
+// 995. Create a function that takes an array of card numbers and checks if the sum of their value exceeds 21. If the sum exceeds 21, return true and if the sum is under or equal to 21, return false. Values of the cards are as follows:
 
+// 2-10 are their value.
+// J-K (face cards) count as 10.
+// Aces count either as 1 or 11 - play conservatively, so that if giving an ace a value of 11 causes you to lose and 1 allows you to win, then go with 1.
+
+// Yellow - overTwentyOne([2, 8, "J"]) ➞ false
+// overTwentyOne(["A", "J", "K"]) ➞ false
+// overTwentyOne([5, 5, 3, 9]) ➞ true
+// overTwentyOne([2, 6, 4, 4, 5]) ➞ false
+// overTwentyOne(["J", "Q", "K"]) ➞ true
+
+const overTwentyOne = cards => {
+	return cards.reduce((total, card) => {
+		if (['J', 'Q', 'K'].includes(card)) card = 10; 
+		if (card === 'A') card = 1; 
+		return total + card; 
+	}, 0) > 21; 
+}; 
+
+function overTwentryOne(cards) {
+  return cards.reduce((a, b) => {
+		if (b === 'A') {
+			return a + 1
+		} else if (b === 'J' || b === 'Q' || b === 'K') {
+			return a + 10
+		} else {
+			return a + b 
+		}
+	}, 0) > 21
+}
+
+function overTwentyOne(cards) {
+	return cards
+	.map(x => x == 'A' ? 1 : typeof x == 'string' ? 10 : x)
+	.reverse()
+	.reduce((a,c) => c == 1 && a + 11 < 21 ? a + 11 : a + c,0 )
+> 21;
+}
