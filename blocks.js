c375d27c519382238220a6fab7e477d576cba651
tindoann <tindoann@gamil.com>
2020-02-19 20:13:11 -0500

    Day75d - sudoku

--- blocks.js
+++ blocks.js
@@ -12799,3 +12799,39 @@ function perrin(n) {
 	}
 	return arr[n]; 
 }
+
+// 997 A Sudoku is a 9x9 grid that is completed when every 3x3 square, row and column consist of the numbers 1-9.
+// For this task, you will be given a completed 3x3 square, in the form of a two-dimensional array. Create a function that checks to make sure this 3x3 square contains each number from 1-9 exactly once. Make sure there are no duplicates, and no numbers outside this range.
+
+// isMiniSudoku([[1, 3, 2], [9, 7, 8], [4, 5, 6]]) ➞ true
+
+// isMiniSudoku([[1, 1, 3], [6, 5, 4], [8, 7, 9]]) ➞ false
+// The 1 is repeated twice 
+
+// isMiniSudoku([[0, 1, 2], [6, 4, 5], [9, 8, 7]]) ➞ false
+// The 0 is included (outside range)
+
+// isMiniSudoku([[8, 9, 2], [5, 6, 1], [3, 7, 4]]) ➞ true 
+
+
+function isMiniSudoku(square) {
+	let a = square.join(); 
+	for (let i = 1; i <= 9; i++) {
+		if (a.indexOf(i) == -1) {
+			return false; 
+		} else {
+			return true; 
+		}
+	}
+}
+
+function isMiniSudoku(square) {
+	return square[0].concat(square[1],square[2]).sort().join() == '1,2,3,4,5,6,7,8,9'
+}
+
+function isMiniSudoku(square) {
+	let arr = []; 
+	square.forEach(a => arr = arr.concat(a)); 
+	return arr.sort().every((entry, id, array) => entry === id + 1); 
+}
+
