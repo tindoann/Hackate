a55fd0ecf85387471d59557ce27ec493b49ab982
tindoann <tindoann@gamil.com>
2020-02-23 17:41:27 -0500

    Day77a - Yellow

--- blocks.js
+++ blocks.js
@@ -12949,4 +12949,34 @@ function unique(arr) {
 	}
 }
 
+// 1002. Create the instance properties fullname and email in the Employee class. Given a person's first and last names:
+// Form the fullname by simply joining the first and last name together, separated by a space.
+// Form the email by joining the first and last name together with a . in between, and follow it with @company.com at the end. Make sure everything is in lowercase.
 
+// emp1 = Employee('John', 'Smith')
+// emp1.fullname ➞ 'John Smith'
+
+// emp2 = Employee('Mary',  'Sue')
+// emp2.email ➞ 'mary.sue@company.com'
+
+// emp3 = Employee('Antony', 'Walker')
+// emp3.firstname ➞ 'Antony'
+
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		// Complete the code!
+		this.fullname = `${firstname} ${lastname}`;
+		this.email = `${firstname}.${lastname}@company.com`.toLowerCase();
+	}
+}
+
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.fullname = firstname + ' ' + lastname
+		this.email = (firstname + '.' + lastname + '@company.com').toLowerCase()
+	}
+}
