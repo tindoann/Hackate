eeaf3991d3b6478cf93e25fb8e7f24772b6230a9
tindoann <tindoann@gamil.com>
2020-03-04 20:31:59 -0500

    Day91a - Yellow

--- blocks.js
+++ blocks.js
@@ -13180,3 +13180,57 @@ function isSlidey(n) {
 	}
 	return true;
 }
+
+// 1010. Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+// {other person name} is {older than / younger than / the same age as} me.
+
+// p1 = Person("Samuel", 24)
+// p2 = Person("Joel", 36)
+// p3 = Person("Lily", 24)
+
+// Yellow - p1.compareAge(p2) ➞ "Joel is older than me."
+// p2.compareAge(p1) ➞ "Samuel is younger than me."
+// p1.compareAge(p3) ➞ "Lily is the same age as me."
+
+class Person {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+		
+	compareAge(other) {
+		let i = Math.sign(other.age - this.age),
+				x = ["the same age as", "older than"][i] || "younger than";
+		return `${other.name} is ${x} me.`;
+	}
+}
+
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+		
+	compareAge(other) {
+		if (this.age < other.age){
+			return `${other.name} is older than me.`
+		}else if (this.age > other.age){
+			return `${other.name} is younger than me.`
+		}else{
+			return `${other.name} is the same age as me.`
+		}
+	}
+}
+
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+		
+	compareAge(other) {
+		if (this.age > other.age) return `${other.name} is younger than me.`;
+		if (this.age < other.age) return `${other.name} is older than me.`;
+		return `${other.name} is the same age as me.`;
+	}
+}
