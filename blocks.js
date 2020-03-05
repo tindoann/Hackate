9404de3672e60e5f99c8a622b5cb728ea92d2c68
tindoann <tindoann@gamil.com>
2020-03-05 18:53:41 -0500

    Day91b - White

--- blocks.js
+++ blocks.js
@@ -13234,3 +13234,28 @@ class Person {
 		return `${other.name} is the same age as me.`;
 	}
 }
+
+// 1011. Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+
+// Yellow - addIndexes([0, 0, 0, 0, 0]) ➞ [0, 1, 2, 3, 4]
+// addIndexes([1, 2, 3, 4, 5]) ➞ [1, 3, 5, 7, 9]
+// addIndexes([5, 4, 3, 2, 1]) ➞ [5, 5, 5, 5, 5]
+
+function addIndexex(arr) {
+	return arr.map((val, i) => val + i); 
+}
+
+function addIndexes(arr) {
+	let result = []; 
+	for(let i = 0; i < arr.length; i++) {
+		result.push(i + arr[i])
+	}
+	return result;
+}
+
+function addIndexes(arr) {
+	for(var i = 0; i < arr.length; i++) {
+		arr[i] += i
+	}
+  return arr
+}
