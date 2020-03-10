2ae5937263ca3d3c4e21bdc76c844fd7c3faf1e2
tindoann <tindoann@gamil.com>
2020-03-09 22:56:28 -0400

    Day95a - White

--- blocks.js
+++ blocks.js
@@ -13449,3 +13449,22 @@ function sortByLast(str) {
 			.map(e => e.reverse().join(''))
 			.join(' ')
 }
+
+// 1018. Given an array of math expressions, create a function which sorts the array by their answer. It should be sorted in ascending order.
+
+// White - sortByAnswer(["1 + 1", "1 + 7", "1 + 5", "1 + 4"]) ➞ ["1 + 1", "1 + 4", "1 + 5", "1 + 7"
+// sortByAnswer(["4 - 4", "2 - 2", "5 - 5", "10 - 10"]) ➞ ["4 - 4", "2 - 2", "5 - 5", "10 - 10"]
+// sortByAnswer(["2 + 2", "2 - 2", "2 x 1"]) ➞ ["2 - 2", "2 x 1", "2 + 2"]
+
+const sortByAnswer = arr => {
+	const re = s => s.replace("x", "*");
+	return arr.sort((a,b) => eval(re(a)) - eval(re(b)));
+}
+
+function sortByAnswer(arr) {
+  const calc = exp => eval(exp.replace("x", "*"));
+  return arr.sort((a, b) => calc(a) - calc(b));
+}
+
+const sortByAnswer = arr =>
+			arr.sort((a,b) => eval(a.replace('x','*')) - eval(b.replace('x', '*')));
