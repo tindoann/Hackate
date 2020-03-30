c5af15f062d4255647a1a1a1d274b5ae1c6ccc0d
tindoann <tindoann@gamil.com>
2020-03-29 23:40:08 -0400

    Day112 - Yellow

--- blocks.js
+++ blocks.js
@@ -14281,3 +14281,33 @@ for(i =0; i< arr.length; i += size){
 }
 	return z;
 }
+
+// 112. Given an array of users, each defined by an object with the following properties: name, score, reputation create a function that sorts the array to form the correct leaderboard.
+// The leaderboard takes into consideration the score of each user of course, but an emphasis is put on their reputation in the community, so to get the trueScore, you should add the reputation multiplied by 2 to the score.
+// Once you know the trueScore of each user, sort the array according to it in descending order.
+
+
+// White - leaderboards([
+//   { name: "a", score: 100, reputation: 20 },
+//   { name: "b", score: 90, reputation: 40 },
+//   { name: "c", score: 110, reputation: 30 },
+// ]) ➞ [
+//   { name: "c", score: 115, reputation: 30 }, // trueScore = 175
+//   { name: "b", score: 90, reputation: 40 },  // trueScore = 170
+//   { name: "a", score: 100, reputation: 20 }  // trueScore = 140
+// ]
+
+function leaderboards(users) {
+	return users.sort((a,b) => (b.score + (b.reputation*2)) - (a.score + (a.reputation*2)))
+}
+
+function leaderboards(users) {
+	return users.sort((u1, u2) => {
+		return (u2.score + u2.reputation * 2) - (u1.score + u1.reputation * 2);
+	});
+}
+
+function leaderboards(users) {
+	return users
+		.sort((a, b) => (b.score + b.reputation * 2) - (a.score + a.reputation * 2))
+}
