a32d9b7297350d76b4f43f61926ef6007eff02c4
tindoann <tindoann@gamil.com>
2020-04-07 23:36:28 -0400

    Day119 - White

--- blocks.js
+++ blocks.js
@@ -14521,3 +14521,29 @@ function lcm(a, b) {
 			return a / i * b
 	}
 }
+
+// 1053. Your function will get an array with a number sequence. However, one item will be missing. It's your job to find out which one is not in the array.
+// To illustrate, given the array [1, 3, 4, 5], 2 is missing so the output must be 2.
+
+// White - missing([1, 3, 4, 5]) ➞ 2
+// missing([2, 4, 6, 8, 10, 14, 16]) ➞ 12
+// missing([1.5, 2, 3]) ➞ 2.5
+
+function missing(arr) {
+	var sum = (arr.length + 1) * (arr[0] + arr[arr.length - 1]) / 2;
+	return sum  - arr.reduce((x,y) => x + y);
+}
+
+const missing = arr => {
+	const r = (arr[arr.length - 1] - arr[0] )/ arr.length;
+	let narr = [];
+	arr.map((el,i) => el - arr[i - 1] === r ? el : narr.push(el))
+	return narr[1] - r
+}
+
+const missing = arr => {
+	const seq = (arr[arr.length - 1] - arr[0]) / arr.length
+	return arr.slice(1).find((el, i) => el - arr[i] !== seq) - seq
+}
+
+
