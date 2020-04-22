cd498e34774e51d6f4f4e60c01912b18cc89925f
tindoann <tindoann@gamil.com>
2020-04-21 23:24:33 -0400

    Day132 - Yellow

--- blocks.js
+++ blocks.js
@@ -14969,3 +14969,26 @@ const howBad = num => {
     isPrime(ones) ? 'Pernicious' : []
   );
 };
+
+// 1057. Every quadratic curve y = a x² + b x + c has a vertex point: the turning point where the curve stops heading down and starts going up.
+// Given the values a, b and c, you need to return the coordinates of the vertex. Return your answers rounded to 2 decimal places.
+
+// Yellow - findVertex(1, 0, 25)  ➞ [0, 25]
+// The vertex of y=x²+25 is at (0, 25).
+
+// findVertex(-1, 0, 25) ➞ [0, 25]
+// The vertex of y=-x²+25 is at (0, 25).
+
+// findVertex(1, 10, 4) ➞ [-5, -21]
+// The vertex of y=x²+10x+4 is at (-5, -21).
+
+function findVertex(a, b, c) {
+  let x = -b / (2 * a);
+  let y = a * (x * x) + (b * x) + c;
+  return [x, y];
+}
+
+const findVertex = (a, b, c) => {
+  const x = -b / (2 * a);
+  return [x, a * Math.pow(x, 2) + b * x + c];
+};
