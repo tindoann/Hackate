f6ac700976361f4d5798169dd55b7e9cb9f6ebae
tindoann <tindoann@gamil.com>
2020-04-23 23:43:56 -0400

    Day134 - White

--- blocks.js
+++ blocks.js
@@ -15014,4 +15014,25 @@ function sevenBoom(arr) {
 		.includes('7') ? "Boom!" : "there is no 7 in the array"
 }
 
+// 1068. In this challenge, you have to find the numeric value of a given word. Instead of the usual sum of Unicode values, you have to convert the whole word into a decimal number from a base equal to ten plus the position in the alphabet of the "highest" letter of the word (counting from a = 1 to z = 26).
+// Given a string word, implement a function that returns an integer being the decimal value obtained after the conversion from the word specific base, accordingly to the instructions above.
 
+wordToDecimal("Edabit") ➞ 351010469
+// The highest letter of "Edabit" in the alphabet is "T"
+// "T" is the 20th letter of the alphabet: adding 10 the result is 30
+// "Edabit" in base30 is equal to 351010469 in base10
+
+wordToDecimal("JavaScript") ➞ 680575028284221
+// The highest letter of "JavaScript" in the alphabet is "v"
+// "v" is the 22th letter of the alphabet: adding 10 the result is 32
+// "JavaScript" in base32 is equal to 680575028284221 in base10
+
+const wordToDecimal = word => {
+	let base = [...word.toLowerCase()].sort()[word.length - 1];
+	return parseInt(word, base.charCodeAt(0) - 86);
+}
+
+const wordToDecimal = str => {
+	let base = str.toLowerCase().split('').sort()[str.length-1].charCodeAt()-96+10;
+	 return parseInt(str, base)
+ }
