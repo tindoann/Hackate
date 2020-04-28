4945c1695fa6a0ec2482f8ae3b5164a7461fa9b9
tindoann <tindoann@gamil.com>
2020-04-27 23:54:57 -0400

    Day136 - White

--- blocks.js
+++ blocks.js
@@ -15081,5 +15081,19 @@ function recompose(string) {
 								.join("");
 }
 
+// 1070. Create a function that determines whether a number is Oddish or Evenish. A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even. If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
+// For example, oddishOrEvenish(121) should return "Evenish", since 1 + 2 + 1 = 4. oddishOrEvenish(41) should return "Oddish", since 4 + 1 = 5.
+
+// White - oddishOrEvenish(43) ➞ "Oddish"
+// oddishOrEvenish(373) ➞ "Oddish"
+// oddishOrEvenish(4433) ➞ "Evenish"
+
+function oddishOrEvenish(num) {
+	return [...String(num)].map(Number).reduce((a,v) => a + v) % 2 ? 'Oddish' : 'Evenish'
+}
+
+function oddishOrEvenish(num) {
+	return `${num}`.split('').reduce((a, b)=>a*1+b*1)%2==0?'Evenish':'Oddish'
+ }
 
 
