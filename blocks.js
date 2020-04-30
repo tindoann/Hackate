e96e91ff64f29a73f82e77189ce80c06a5e687f8
tindoann <tindoann@gamil.com>
2020-04-29 23:40:00 -0400

    Day137 - White

--- blocks.js
+++ blocks.js
@@ -15096,4 +15096,35 @@ function oddishOrEvenish(num) {
 	return `${num}`.split('').reduce((a, b)=>a*1+b*1)%2==0?'Evenish':'Oddish'
  }
 
+ // 1071. The .length property on an array will return the number of elements in the array. For example, the array below contains 2 elements:
 
+ // [1, [2, 3]]
+// 2 elements, number 1 and array [2, 3]
+
+// Suppose we instead wanted to know the total number of non-nested items in the nested array. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
+// Write a function that returns the total number of non-nested items in a nested array.
+
+// getLength([1, [2, 3]]) ➞ 3
+// getLength([1, [2, [3, 4]]]) ➞ 4
+// getLength([1, [2, [3, [4, [5, 6]]]]]) ➞ 6
+// getLength([1, [2], 1, [2], 1]) ➞ 5
+
+function getLength(arr) {
+	return arr.flat(Infinity).length;
+}
+
+function getLength(arr) {
+  return arr.length === 0 ? 0 : JSON.stringify(arr).match(/\w/g).length || 0;
+}
+
+function getLength(arr) {
+	let count = 0;
+	arr.forEach(item => {
+		if (Array.isArray(item)) {
+			count += getLength(item);
+		} else {
+			count++;
+		}
+	});
+	return count;
+}
